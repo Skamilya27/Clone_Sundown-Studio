632eafc2e015d0207b64b92e8d4c4ef64cf7ce49
Suman <sumankamilya27@gmail.com>
2024-01-17 00:34:22 +0530

    footer animation and slider animation

--- script.js
+++ script.js
@@ -2,3 +2,34 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function page3Animation() {
+    let elemC = document.querySelector('#elem-container');
+let fixedImage = document.querySelector('#fixed-image'); 
+elemC.addEventListener("mouseenter", () => {
+    fixedImage.style.display = 'block';
+})
+elemC.addEventListener("mouseleave", () => {
+    fixedImage.style.display = 'none';
+})
+
+let elems = document.querySelectorAll(".elem");
+elems.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
+        let image = e.getAttribute("data-image");
+        fixedImage.style.backgroundImage = `url(${image})`
+    })
+})
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+
+page3Animation();
+swiperAnimation();
+
